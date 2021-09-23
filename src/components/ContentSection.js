de42bcc5d13aec80565001a6cb876bb69c61a1d2
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 08:51:56 +0000

    ContentSection.js: changed

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -12,7 +12,10 @@ export default class ContentSection extends React.Component {
                 {_.get(section, 'title', null) && (
                 <h2 className="section__title line-top">{_.get(section, 'title', null)}</h2>
                 )}
-                
+                  {_.get(section, 'image', null) && (
+                <figure className="section__image">
+                  <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
+               
                 {_.get(section, 'subtitle', null) && (
                 <p className="section__subtitle">{_.get(section, 'subtitle', null)}</p>
                 )}
